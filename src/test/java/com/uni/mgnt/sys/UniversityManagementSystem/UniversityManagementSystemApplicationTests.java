8e4c33b91e2458d7f56d3abb30c7eb71de98472a
RokonNazrul <RokonNazrul@DESKTOP-DLUE9HL>
2020-08-22 23:57:22 +0600

    Initial commit

---
+++ src/test/java/com/uni/mgnt/sys/UniversityManagementSystem/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.mgnt.sys.UniversityManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
