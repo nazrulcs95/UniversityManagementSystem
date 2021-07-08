1d31770ad9e4ace0d18f8b246e05b1bd8cb54d22
Nazrul Islam <n.islam.rokon@gmail.com>
2021-07-08 15:49:45 +0600

    Project rebuild by STS

---
+++ src/main/java/com/university_management/contoller/Controller.java
@@ -0,0 +1,37 @@
+package com.university_management.contoller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Controller() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/index.jsp");  
+        rd.include(request, response);
+	}
+
+	
+}
